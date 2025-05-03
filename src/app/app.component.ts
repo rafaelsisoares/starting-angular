f238a16f5d97d8f47e8fbdf1fa8ceaab50e0872b
Rafael Soares <r.soares2015@outlook.com>
2025-05-02 21:25:10 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./components/home/home.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-start',
+  imports: [RouterOutlet, HomeComponent],
+  // templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  template: `
+    <h1>My First Angular App</h1>
+    <app-home></app-home>
+    <router-outlet />`
 })
 export class AppComponent {
   title = 'starting-angular';
