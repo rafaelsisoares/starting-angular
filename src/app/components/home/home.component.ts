13700f7d465f527d7d0793192571480cec81c9cf
Rafael Soares <r.soares2015@outlook.com>
2025-05-06 20:09:01 -0300

    brincando com signal

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FooterComponent } from "../footer/footer.component";
 
 @Component({
@@ -13,9 +13,22 @@ export class HomeComponent {
   num = 1;
   condition = this.num >= 5 ? "Verdadeiro" : "Falso";
   image = "https://www.w3schools.com/w3images/lights.jpg";
+  counter = signal(0);
 
   handleClick() {
     this.num = 5;
     console.log("Peguei seu click");
+  }
+
+  handleCounterPlus() {
+    this.counter.set(this.counter() + 1);
+  }
+
+  handleCounterMinus() {
+    this.counter.set(this.counter() - 1);
+  }
+
+  resetCounter() {
+    this.counter.set(0);
   }
 };
